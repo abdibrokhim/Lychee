30a063f21c3ec1e2e5e8bdb17ad512d85ccdf2b3
abdibrokhim <abdibrokhim@gmail.com>
2023-02-23 23:49:14 +0500

    Delete youtubeapi.py

--- backend/youtubeapi.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-api_key = "AIzaSyAu3lbrWAoQcfQ-j5eR2efxw2D5jS1Behk"
-video_id = "33Qu3oilteU"
-
-url = f"https://www.googleapis.com/youtube/v3/captions?part=snippet&videoId={video_id}&key={api_key}"
-response = requests.get(url)
-
-print(response.json())
